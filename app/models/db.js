1cb4c6e3d91c9eae557e02dd2524b7b726799413
Ahmed Nouria <ahmnouira@gmail.com>
2023-09-06 15:58:14 +0100

    fix: db connection

--- app/models/db.js
+++ app/models/db.js
@@ -1,19 +1,7 @@
 const mongoose = require("mongoose");
 
-/* To fix all deprecation warnings : https://mongoosejs.com/docs/deprecations.html */
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-
-const mongoDB = "mongodb://localhost:3333/MEAN_web_dev";
-
-mongoose.connect(mongoDB, (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
+mongoose.connect("mongodb://localhost:3333/MEAN_web_dev");
 
 mongoose.connection.on("open", () => {
-  console.log("mongoose conneted");
+  console.log("mongoose connected");
 });
